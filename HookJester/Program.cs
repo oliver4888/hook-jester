3476d16295e7fe0b89adbb4dc627383907c364be
Oliver <lutleyo@gmail.com>
2019-03-19 21:33:16 +0000

    Added MVC template & Serilog logging.

---
+++ HookJester/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Serilog;
+
+namespace HookJester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHostBuilder webHostBuilder = CreateWebHostBuilder(args);
+
+            IWebHost webHost = webHostBuilder.Build();
+
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(webHost.Services.GetRequiredService<IConfiguration>())
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                webHost.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .UseUrls("http://localhost:9090")
+                .UseStartup<Startup>();
+    }
+}
